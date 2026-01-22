4525f71dd881d34918152c511c623347f1371d46
A2va <49582555+A2va@users.noreply.github.com>
2026-01-22 18:23:00 +0100

    feat(v1): init logic docker provider

--- go.mod
+++ go.mod
@@ -12,7 +12,9 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/charmbracelet/colorprofile v0.4.1 // indirect
 	github.com/charmbracelet/lipgloss v1.1.0 // indirect
 	github.com/charmbracelet/x/ansi v0.11.3 // indirect
@@ -21,7 +23,15 @@ require (
 	github.com/clipperhouse/displaywidth v0.6.2 // indirect
 	github.com/clipperhouse/stringish v0.1.1 // indirect
 	github.com/clipperhouse/uax29/v2 v2.3.0 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.6.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/schema v1.2.1 // indirect
@@ -34,8 +44,13 @@ require (
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/moby/api v1.52.0 // indirect
+	github.com/moby/moby/client v0.2.1 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/muhlemmer/gu v0.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/pkg/sftp v1.13.10 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
@@ -45,7 +60,10 @@ require (
 	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/zitadel/oidc/v2 v2.12.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
 	go.opentelemetry.io/otel v1.39.0 // indirect
+	go.opentelemetry.io/otel/metric v1.39.0 // indirect
 	go.opentelemetry.io/otel/trace v1.39.0 // indirect
 	golang.org/x/crypto v0.46.0 // indirect
 	golang.org/x/exp v0.0.0-20251209150349-8475f28825e9 // indirect
---
+++ pkg/backend/v1/docker/init.go
@@ -0,0 +1,262 @@
+package docker
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/A2va/lsw/pkg/backend"
+	"github.com/A2va/lsw/pkg/config"
+	"github.com/charmbracelet/log"
+	"github.com/containerd/errdefs"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/api/types/network"
+	"github.com/moby/moby/client"
+)
+
+func getDockerfile() (string, error) {
+	log.Debug("get dockerfile")
+
+	version := config.GetVersion()
+	url := fmt.Sprintf("https://raw.githubusercontent.com/A2va/lsw/%s/Dockerfile.v1", version.Commit)
+
+	var dockerfilePath string
+	if version.Version == "dev" {
+		wd, _ := os.Getwd()
+		dockerfilePath = path.Join(wd, "Dockerfile.v1")
+	} else {
+		_, err := backend.DownloadFileIfNeeded(url, "Dockerfile.v1")
+		if err != nil {
+			return "", err
+		}
+
+		cache, err := backend.GetCacheDir()
+		if err != nil {
+			return "", err
+		}
+		dockerfilePath = path.Join(cache, "downloads", "Dockerfile.v1")
+	}
+
+	log.Debug("File path", "dockerfile", dockerfilePath)
+
+	return dockerfilePath, nil
+}
+
+func createBuildContext(dockerfilePath string) (io.Reader, error) {
+	log.Debug("create build context")
+
+	// Read the Dockerfile content
+	body, err := os.ReadFile(dockerfilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a buffer to write our archive to
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+	defer tw.Close()
+
+	// Create a header for the "Dockerfile" file inside the tar
+	header := &tar.Header{
+		Name:    "Dockerfile", // The name the daemon will see
+		Size:    int64(len(body)),
+		Mode:    0644,
+		ModTime: time.Now(),
+	}
+
+	// Write the header and the content
+	if err := tw.WriteHeader(header); err != nil {
+		return nil, err
+	}
+	if _, err := tw.Write(body); err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
+
+type ContainerMigration struct {
+	ContainerInfo       []container.InspectResponse
+	OldImagesWasRemoved bool
+}
+
+// Remove old containers and images, return container info
+func pruneOldVersions(c *client.Client) (ContainerMigration, error) {
+	f := make(client.Filters).Add("reference", "lsw-v1:*")
+
+	res, err := c.ImageList(context.Background(), client.ImageListOptions{Filters: f})
+	if err != nil {
+		return ContainerMigration{}, nil
+	}
+
+	version := config.GetVersion()
+	currentTag := fmt.Sprintf("lsw-v1:%s", version.ShortCommit)
+
+	oldWasRemoved := false
+	inspectContainers := []container.InspectResponse{}
+
+	images := res.Items
+	for _, image := range images {
+		isOldVersion := false
+		isCurrentVersion := false
+
+		tags := image.RepoTags
+		for _, tag := range tags {
+			if tag == "<none>:<none>" {
+				continue
+			}
+
+			if tag == currentTag {
+				isCurrentVersion = true
+			}
+
+			// Check if it is an lsw image but NOT the current one
+			if strings.HasPrefix(tag, "lsw-v1:") && tag != currentTag {
+				isOldVersion = true
+			}
+
+		}
+
+		if isOldVersion && !isCurrentVersion {
+			log.Debug("old image found", "id", image.ID)
+
+			f := make(client.Filters).Add("ancestor", image.ID)
+			res, err := c.ContainerList(context.Background(), client.ContainerListOptions{Filters: f, All: true})
+			if err != nil {
+				return ContainerMigration{}, err
+			}
+
+			// It should be safe to delete the container since we use volume to store data
+			containers := res.Items
+			for _, container := range containers {
+				log.Debug("containers runing on old image", "id", container.ID)
+
+				res, err := c.ContainerInspect(context.Background(), container.ID, client.ContainerInspectOptions{})
+				if err != nil {
+					return ContainerMigration{}, err
+				}
+
+				inspectContainers = append(inspectContainers, res.Container)
+
+				_, err = c.ContainerRemove(context.Background(), container.ID, client.ContainerRemoveOptions{Force: true})
+				if err != nil {
+					return ContainerMigration{}, err
+				}
+
+				log.Debug("prune container", "id", container.ID)
+			}
+
+			// Delete image
+			oldWasRemoved = true
+
+			log.Debug("remove old image", "id", image.ID)
+			_, err = c.ImageRemove(context.Background(), image.ID, client.ImageRemoveOptions{Force: true, PruneChildren: true})
+			if err != nil {
+				return ContainerMigration{}, err
+			}
+		}
+	}
+
+	return ContainerMigration{OldImagesWasRemoved: oldWasRemoved, ContainerInfo: inspectContainers}, nil
+}
+
+func buildImage(c *client.Client) error {
+	version := config.GetVersion()
+	targetTag := fmt.Sprintf("lsw-v1:%s", version.ShortCommit)
+
+	// Build the image if there isn't already one
+	if version.Version != "dev" {
+		_, err := c.ImageInspect(context.Background(), targetTag)
+		if err == nil {
+			// No error = Image exists.
+			log.Debug("image already exists. skipping build.")
+			return nil
+		}
+
+		if !errdefs.IsNotFound(err) {
+			return fmt.Errorf("failed to check image existence: %w", err)
+		}
+	}
+
+	noCache := true
+	remove := true
+	// Use caching when developing the project to have a better iteration
+	if version.Version == "dev" {
+		noCache = false
+		remove = false
+	}
+
+	log.Debug("build image with conf:", "nocache", noCache, "remove", remove, "tag", targetTag)
+
+	buildOptions := client.ImageBuildOptions{
+		NoCache: noCache,
+		Remove:  remove,
+		Tags:    []string{targetTag},
+	}
+
+	dockerfilePath, err := getDockerfile()
+	if err != nil {
+		panic(err)
+	}
+
+	buildContext, err := createBuildContext(dockerfilePath)
+	if err != nil {
+		return err
+	}
+
+	res, err := c.ImageBuild(context.Background(), buildContext, buildOptions)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	// Ensure the build actually finished/succeeded by reading the output
+	// If you don't read the body, the build might be cancelled or hang
+	// TODO Logs the build output (at least in debug mode)
+	_, err = io.Copy(io.Discard, res.Body)
+	// _, err = io.Copy(os.Stdout, res.Body)
+	return nil
+}
+
+func Init() {
+
+	c, err := client.New(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	migration, err := pruneOldVersions(c)
+	if err != nil {
+		panic(err)
+	}
+
+	err = buildImage(c)
+	if err != nil {
+		panic(err)
+	}
+
+	if migration.OldImagesWasRemoved {
+		log.Debug("restore pruned containers")
+		version := config.GetVersion()
+		image := fmt.Sprintf("lsw-v1:%s", version.ShortCommit)
+		for _, container := range migration.ContainerInfo {
+			// Create the same containers using the new image
+			container.Config.Image = image
+
+			createOptions := client.ContainerCreateOptions{
+				Name:             container.Name,
+				Config:           container.Config,
+				HostConfig:       container.HostConfig,
+				NetworkingConfig: &network.NetworkingConfig{EndpointsConfig: container.NetworkSettings.Networks},
+			}
+			c.ContainerCreate(context.Background(), createOptions)
+		}
+	}
+}
