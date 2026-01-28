98fa3f1d2b996a6cacfbdf362d1c90e58d6e87d6
A2va <49582555+A2va@users.noreply.github.com>
2026-01-28 20:57:19 +0100

    chore: remove autounattend folder

--- autounattend/scripts/specialize.ps1
+++
@@ -1,3 +0,0 @@
-# Bypass Windows 11 Network Requirement
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassNRO" /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "BypassNRO" /t REG_DWORD /d 1 /f
