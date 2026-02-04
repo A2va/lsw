bd39d813ff72b3c881851e287bf3a7625c5a058f
A2va <49582555+A2va@users.noreply.github.com>
2026-02-04 18:43:32 +0100

    fix(v1): start and stop is now unused in v1

--- pkg/backend/v1/docker/start.go
+++ pkg/backend/v1/docker/start.go
@@ -1,25 +1,9 @@
 package docker
 
 import (
-	"context"
-
 	"github.com/A2va/lsw/pkg/config"
-	"github.com/charmbracelet/log"
-	"github.com/moby/moby/client"
 )
 
 func Start(bottle config.Bottle) error {
-	log.Debug("start container using docker provider", "name", bottle.Name)
-
-	c, err := client.New(client.FromEnv)
-	if err != nil {
-		return err
-	}
-
-	log.Debug("start v1 bottle", "name", bottle.Name)
-	_, err = c.ContainerStart(context.Background(), bottle.Name, client.ContainerStartOptions{})
-	if err != nil {
-		return err
-	}
 	return nil
 }
--- pkg/backend/v1/podman/start.go
+++ pkg/backend/v1/podman/start.go
@@ -1,26 +1,9 @@
 package podman
 
 import (
-	"context"
-
 	"github.com/A2va/lsw/pkg/config"
-	"github.com/charmbracelet/log"
-	"github.com/containers/podman/v6/pkg/bindings"
-	"github.com/containers/podman/v6/pkg/bindings/containers"
 )
 
 func Start(bottle config.Bottle) error {
-	log.Debug("start container using podman provider", "name", bottle.Name)
-
-	c, err := bindings.NewConnection(context.Background(), "unix:///run/podman/podman.sock")
-	if err != nil {
-		return err
-	}
-
-	err = containers.Start(c, bottle.Name, &containers.StartOptions{})
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
