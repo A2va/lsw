693ffe568107d955f38207db7d2c9cf5c5af343b
A2va <49582555+A2va@users.noreply.github.com>
2026-03-14 16:47:21 +0100

    fix(v1): fill volume in new cmd

--- pkg/backend/v1/podman/new.go
+++ pkg/backend/v1/podman/new.go
@@ -79,6 +79,12 @@ func New(name string) error {
 		return err
 	}
 
+	// We just need the container to start so Podman triggers the volume copy-up.
+	spec.Command = []string{"true"}
+	f := false
+	spec.Terminal = &f
+	spec.Stdin = &f
+
 	res, err := containers.CreateWithSpec(c, &spec, &containers.CreateOptions{})
 	if err != nil {
 		return err
@@ -86,7 +92,21 @@ func New(name string) error {
 
 	// Remove the container because all we wanted was to create a new volume
 	// From there container creation are faster, so that we can mount new path easily be recreating the container (they are immutable)
-	_, err = containers.Remove(c, res.ID, &containers.RemoveOptions{})
+	defer func() {
+		containers.Remove(c, res.ID, new(containers.RemoveOptions).WithForce(true))
+	}()
+
+	err = containers.Start(c, res.ID, &containers.StartOptions{})
+	if err != nil {
+		return err
+	}
+
+	waitOpts := new(containers.WaitOptions).WithCondition([]define.ContainerStatus{
+		define.ContainerStateExited,
+		define.ContainerStateStopped,
+	})
+
+	_, err = containers.Wait(c, res.ID, waitOpts)
 	if err != nil {
 		return err
 	}
